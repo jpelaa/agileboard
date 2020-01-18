7cea80bb2cba6c79e6384b3d2c16b9869ab923cb
Jayaprakash Elavarasan <jayaprakash@solbaacken.com>
2020-01-18 11:31:29 +0530

    Updates

---
+++ precache-manifest.8efec42764f97250f4ccf5d3144fa5f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2b6dc6a1f3a81575a1dfe10c1f14a8",
+    "url": "/agileboard.github.io/index.html"
+  },
+  {
+    "revision": "1c015af6c5de2f19d9c3",
+    "url": "/agileboard.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58a2a1f7a0bd274c5e2a",
+    "url": "/agileboard.github.io/static/js/2.c962cd91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/agileboard.github.io/static/js/2.c962cd91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c015af6c5de2f19d9c3",
+    "url": "/agileboard.github.io/static/js/main.265e70b5.chunk.js"
+  },
+  {
+    "revision": "bb0cbdc5f1061951c5d1",
+    "url": "/agileboard.github.io/static/js/runtime-main.c06844fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/agileboard.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
